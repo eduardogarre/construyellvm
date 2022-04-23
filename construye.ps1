25605a15e022b4c4c6e4965bff3e88b49a287eba
eduardogarre <eduardo.garre@gmail.com>
2022-04-23 13:36:07 +0200

    Escojo versión de llvm

--- construye.ps1
+++ construye.ps1
@@ -1,12 +1,16 @@
-rm llvm -Force -Recurse -ErrorAction SilentlyContinue
-rm obra -Force -Recurse -ErrorAction SilentlyContinue
+#rm llvm-src -Force -Recurse -ErrorAction SilentlyContinue
+#rm obra -Force -Recurse -ErrorAction SilentlyContinue
 
-git clone --depth 1 --config core.autocrlf=false https://github.com/llvm/llvm-project.git llvm-src
+git clone --config core.autocrlf=false https://github.com/llvm/llvm-project.git llvm-src
+
+cd .\llvm-src
+git checkout llvmorg-14.0.0
+cd ..
 
 mkdir obra
 cd obra
 
-cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS=lld -DCMAKE_INSTALL_PREFIX=c:/llvm -DLLVM_TARGETS_TO_BUILD="all" -DLLVM_TARGET_ARCH="host" -DLLVM_BUILD_EXAMPLES=ON -DLLVM_BUILD_TOOLS=ON -DLLVM_ENABLE_THREADS=ON -DLLVM_INCLUDE_EXAMPLES=ON -DLLVM_INCLUDE_TOOLS=ON -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_PARALLEL_COMPILE_JOBS="16" -DLLVM_PARALLEL_LINK_JOBS="16" ..\llvm-src\llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="lld" -DCMAKE_INSTALL_PREFIX=c:/llvm -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;WebAssembly;X86" -DLLVM_TARGET_ARCH="host" -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TOOLS=ON -DLLVM_ENABLE_THREADS=ON -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_INCLUDE_TOOLS=ON -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_PARALLEL_COMPILE_JOBS="24" -DLLVM_PARALLEL_LINK_JOBS="24" ..\llvm-src\llvm
 cmake --build . --parallel
 cmake --build . --target install
 
